dbbdf4ddbbf41c2720ac59ee478b2071c23bcf66
tranthuong021001 <tranthuong021001@gmail.com>
2021-11-05 22:14:45 +0700

    complete module 6

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -18,8 +18,10 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 
-		<link rel="profile" href="https://gmpg.org/xfn/11">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
+		<link rel="profile" href="https://gmpg.org/xfn/11">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/module6.css">
 		<?php wp_head(); ?>
 
 	</head>
--- wp-content/themes/twentytwenty/template-parts/content.php
+++ wp-content/themes/twentytwenty/template-parts/content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The default template for displaying content
  *
@@ -11,41 +12,59 @@
  * @since Twenty Twenty 1.0
  */
 
+$detailPage = "";
+if (is_single()) {
+	$detailPage = "detail-page";
+}
+
 ?>
 
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class($detailPage); ?> id="post-<?php the_ID(); ?>">
 
 	<?php
 
-	get_template_part( 'template-parts/entry-header' );
+	get_template_part('template-parts/entry-header');
 
-	if ( ! is_search() ) {
-		get_template_part( 'template-parts/featured-image' );
+	if (!is_search()) {
+		get_template_part('template-parts/featured-image');
 	}
-
+	if (is_single()) {
 	?>
+		<div class="container">
+			<div class="overviewline"></div>
+		<?php } ?>
+		<div class="post-inner <?php echo is_page_template('templates/template-full-width.php') ? '' : 'thin'; ?> ">
 
-	<div class="post-inner <?php echo is_page_template( 'templates/template-full-width.php' ) ? '' : 'thin'; ?> ">
+			<div class="entry-content">
 
-		<div class="entry-content">
+				<?php
+				if (is_search() || !is_singular() && 'summary' === get_theme_mod('blog_content', 'full')) {
+					the_excerpt();
+				} else {
+					the_content(__('Continue reading', 'twentytwenty'));
+				}
+				?>
 
+			</div><!-- .entry-content -->
 			<?php
-			if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
-				the_excerpt();
-			} else {
-				the_content( __( 'Continue reading', 'twentytwenty' ) );
+			if (is_single()) {
+				//article source
+				printf(/* translators: %s: Author name. */
+					'<p class ="article-source">(Theo ' . esc_html(get_the_author_meta('display_name')) . ')</p>'
+				);
 			}
+
 			?>
 
-		</div><!-- .entry-content -->
-
-	</div><!-- .post-inner -->
-
+		</div><!-- .post-inner -->
+		<?php if (is_single()) { ?>
+		</div>
+	<?php } ?>
 	<div class="section-inner">
 		<?php
 		wp_link_pages(
 			array(
-				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Page', 'twentytwenty' ) . '"><span class="label">' . __( 'Pages:', 'twentytwenty' ) . '</span>',
+				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__('Page', 'twentytwenty') . '"><span class="label">' . __('Pages:', 'twentytwenty') . '</span>',
 				'after'       => '</nav>',
 				'link_before' => '<span class="page-number">',
 				'link_after'  => '</span>',
@@ -55,12 +74,11 @@
 		edit_post_link();
 
 		// Single bottom post meta.
-		twentytwenty_the_post_meta( get_the_ID(), 'single-bottom' );
+		twentytwenty_the_post_meta(get_the_ID(), 'single-bottom');
 
-		if ( post_type_supports( get_post_type( get_the_ID() ), 'author' ) && is_single() ) {
+		if (post_type_supports(get_post_type(get_the_ID()), 'author') && is_single()) {
 
-			get_template_part( 'template-parts/entry-author-bio' );
-
+			get_template_part('template-parts/entry-author-bio');
 		}
 		?>
 
@@ -68,18 +86,17 @@
 
 	<?php
 
-	if ( is_single() ) {
+	if (is_single()) {
 
-		get_template_part( 'template-parts/navigation' );
-
+		get_template_part('template-parts/navigation');
 	}
 
 	/*
 	 * Output comments wrapper if it's a post, or if comments are open,
 	 * or if there's a comment number – and check for password.
 	 */
-	if ( ( is_single() || is_page() ) && ( comments_open() || get_comments_number() ) && ! post_password_required() ) {
-		?>
+	if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) {
+	?>
 
 		<div class="comments-wrapper section-inner">
 
@@ -87,7 +104,7 @@
 
 		</div><!-- .comments-wrapper -->
 
-		<?php
+	<?php
 	}
 	?>
 
