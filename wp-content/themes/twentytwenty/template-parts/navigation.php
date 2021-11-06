99ba7e2dfd4bb94f9519154cbb87f7bbd08f1e44
tranthuong021001 <tranthuong021001@gmail.com>
2021-11-06 14:46:17 +0700

    customize pagination

--- wp-content/themes/twentytwenty/template-parts/navigation.php
+++ wp-content/themes/twentytwenty/template-parts/navigation.php
@@ -24,67 +24,91 @@ if ($next_post || $prev_post) {
 ?>
 
 	<nav class="pagination-single section-inner pagination-single-page<?php echo esc_attr($pagination_classes); ?>" aria-label="<?php esc_attr_e('Post', 'twentytwenty'); ?>" role="navigation">
+		<div class="container">
+			<hr class="styled-separator is-style-wide" aria-hidden="true" />
 
-		<hr class="styled-separator is-style-wide" aria-hidden="true" />
-
-		<div class="pagination-single-inner list_news">
-			<div class="headlines">
-				<ul>
-					<li>
-						<div class="headlinesdate">
-							<div class="headlinesdm">
-								<div class="headlinesday">05</div>
-								<div class="headlinesmonth">11</div>
-							</div>
-							<div class="headlinesyear">21</div>
-						</div>
-						<div class="headlinestitle">
-							<?php
-							if ($prev_post) {
-							?>
-
-								<a class="previous-post link-pagination-title" href="<?php echo esc_url(get_permalink($prev_post->ID)); ?>">
-									<!-- <span class="arrow" aria-hidden="true">&larr;</span> -->
-									<span class="title"><span class="title-inner"><?php echo wp_kses_post(get_the_title($prev_post->ID)); ?></span></span>
-								</a>
-
-							<?php
-							}
-
-							if ($next_post) {
-							?>
-						</div>
-
-					</li>
-					<li>
-						<div class="headlinesdate">
-							<div class="headlinesdm">
-								<div class="headlinesday">05</div>
-								<div class="headlinesmonth">11</div>
-							</div>
-							<div class="headlinesyear">21</div>
-						</div>
-						<div class="headlinestitle">
-							<a class="next-post next-post-single link-pagination-title" href="<?php echo esc_url(get_permalink($next_post->ID)); ?>">
-								<!-- <span class="arrow" aria-hidden="true">&rarr;</span> -->
-								<span class="title"><span class="title-inner"><?php echo wp_kses_post(get_the_title($next_post->ID)); ?></span></span>
-							</a>
+			<div class="pagination-single-inner list_news">
+				<div class="headlines">
+					<ul>
 						<?php
-							}
+						if ($next_post) {
 						?>
-						</div>
+							<li>
+								<div class="headlinesdate">
+									<div class="headlinesdm">
+										<?php
+										$nextPostDateTime = get_post($next_post->ID);
 
-					</li>
-				</ul>
-			</div>
+										$monthNextPost = date("m", strtotime($nextPostDateTime->post_date));
+										$dayNextPost = date("d", strtotime($nextPostDateTime->post_date));
+										$yearNextPost = date("y", strtotime($nextPostDateTime->post_date));
+										?>
+										<div class="headlinesday"><?php echo $dayNextPost
+																								?></div>
+										<div class="headlinesmonth"><?php echo $monthNextPost
+																									?></div>
+									</div>
+									<div class="headlinesyear"><?php echo $yearNextPost
+																							?></div>
+								</div>
+								<div class="headlinestitle">
+
+
+
+									<a class="next-post next-post-single link-pagination-title" href="<?php echo esc_url(get_permalink($next_post->ID)); ?>">
+										<!-- <span class="arrow" aria-hidden="true">&rarr;</span> -->
+										<span class="title"><span class="title-inner"><?php echo wp_kses_post(get_the_title($next_post->ID)); ?></span></span>
+									</a>
+								</div>
+
+							</li>
+						<?php
+						}
+
+						if ($prev_post) {
+						?>
+
+							<li>
+								<div class="headlinesdate">
+									<div class="headlinesdm">
+										<?php
+										$prevPostDateTime = get_post($prev_post->ID);
+
+										$monthPrevPost = date("m", strtotime($prevPostDateTime->post_date));
+										$dayPrevPost = date("d", strtotime($prevPostDateTime->post_date));
+										$yearPrevPost = date("y", strtotime($prevPostDateTime->post_date));
+										?>
+										<div class="headlinesday"><?php echo $dayPrevPost
+																								?></div>
+										<div class="headlinesmonth"><?php echo $monthPrevPost
+																									?></div>
+									</div>
+									<div class="headlinesyear"><?php echo $yearPrevPost
+																							?></div>
+								</div>
+								<div class="headlinestitle">
+									<a class="previous-post link-pagination-title" href="<?php echo esc_url(get_permalink($prev_post->ID)); ?>">
+										<!-- <span class="arrow" aria-hidden="true">&larr;</span> -->
+										<span class="title"><span class="title-inner"><?php echo wp_kses_post(get_the_title($prev_post->ID)); ?></span></span>
+									</a>
+								<?php
+							}
+								?>
+								</div>
+
+							</li>
+					</ul>
+				</div>
 
 
 
 
 
-		</div><!-- .pagination-single-inner -->
+			</div><!-- .pagination-single-inner -->
 
-		<hr class="styled-separator is-style-wide" aria-hidden="true" />
+			<hr class="styled-separator is-style-wide" aria-hidden="true" />
+		</div>
+
 
 	</nav><!-- .pagination-single -->
 
