2ec368d56682766fb36ef184baa2d3fb2dbbde60
tranthuong021001 <56185099+tranthuong021001@users.noreply.github.com>
2021-11-06 08:08:44 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress_h' );
+define( 'DB_NAME', 'help' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
