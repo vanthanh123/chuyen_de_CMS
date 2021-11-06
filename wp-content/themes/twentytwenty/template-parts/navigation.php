70f584792a92607941a43439a71142cf4cb2b28b
tranthuong021001 <tranthuong021001@gmail.com>
2021-11-06 10:39:55 +0700

    complete 80% module7

--- wp-content/themes/twentytwenty/template-parts/navigation.php
+++ wp-content/themes/twentytwenty/template-parts/navigation.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Displays the next and previous post navigation in single posts.
  *
@@ -10,46 +11,76 @@
 $next_post = get_next_post();
 $prev_post = get_previous_post();
 
-if ( $next_post || $prev_post ) {
+if ($next_post || $prev_post) {
 
 	$pagination_classes = '';
 
-	if ( ! $next_post ) {
+	if (!$next_post) {
 		$pagination_classes = ' only-one only-prev';
-	} elseif ( ! $prev_post ) {
+	} elseif (!$prev_post) {
 		$pagination_classes = ' only-one only-next';
 	}
 
-	?>
+?>
 
-	<nav class="pagination-single section-inner<?php echo esc_attr( $pagination_classes ); ?>" aria-label="<?php esc_attr_e( 'Post', 'twentytwenty' ); ?>" role="navigation">
+	<nav class="pagination-single section-inner pagination-single-page<?php echo esc_attr($pagination_classes); ?>" aria-label="<?php esc_attr_e('Post', 'twentytwenty'); ?>" role="navigation">
 
 		<hr class="styled-separator is-style-wide" aria-hidden="true" />
 
-		<div class="pagination-single-inner">
+		<div class="pagination-single-inner list_news">
+			<div class="headlines">
+				<ul>
+					<li>
+						<div class="headlinesdate">
+							<div class="headlinesdm">
+								<div class="headlinesday">05</div>
+								<div class="headlinesmonth">11</div>
+							</div>
+							<div class="headlinesyear">21</div>
+						</div>
+						<div class="headlinestitle">
+							<?php
+							if ($prev_post) {
+							?>
 
-			<?php
-			if ( $prev_post ) {
-				?>
+								<a class="previous-post link-pagination-title" href="<?php echo esc_url(get_permalink($prev_post->ID)); ?>">
+									<!-- <span class="arrow" aria-hidden="true">&larr;</span> -->
+									<span class="title"><span class="title-inner"><?php echo wp_kses_post(get_the_title($prev_post->ID)); ?></span></span>
+								</a>
 
-				<a class="previous-post" href="<?php echo esc_url( get_permalink( $prev_post->ID ) ); ?>">
-					<span class="arrow" aria-hidden="true">&larr;</span>
-					<span class="title"><span class="title-inner"><?php echo wp_kses_post( get_the_title( $prev_post->ID ) ); ?></span></span>
-				</a>
+							<?php
+							}
 
-				<?php
-			}
+							if ($next_post) {
+							?>
+						</div>
 
-			if ( $next_post ) {
-				?>
+					</li>
+					<li>
+						<div class="headlinesdate">
+							<div class="headlinesdm">
+								<div class="headlinesday">05</div>
+								<div class="headlinesmonth">11</div>
+							</div>
+							<div class="headlinesyear">21</div>
+						</div>
+						<div class="headlinestitle">
+							<a class="next-post next-post-single link-pagination-title" href="<?php echo esc_url(get_permalink($next_post->ID)); ?>">
+								<!-- <span class="arrow" aria-hidden="true">&rarr;</span> -->
+								<span class="title"><span class="title-inner"><?php echo wp_kses_post(get_the_title($next_post->ID)); ?></span></span>
+							</a>
+						<?php
+							}
+						?>
+						</div>
 
-				<a class="next-post" href="<?php echo esc_url( get_permalink( $next_post->ID ) ); ?>">
-					<span class="arrow" aria-hidden="true">&rarr;</span>
-						<span class="title"><span class="title-inner"><?php echo wp_kses_post( get_the_title( $next_post->ID ) ); ?></span></span>
-				</a>
-				<?php
-			}
-			?>
+					</li>
+				</ul>
+			</div>
+
+
+
+
 
 		</div><!-- .pagination-single-inner -->
 
@@ -57,5 +88,5 @@ if ( $next_post || $prev_post ) {
 
 	</nav><!-- .pagination-single -->
 
-	<?php
+<?php
 }
